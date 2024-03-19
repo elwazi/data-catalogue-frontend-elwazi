4373281cbdc368c03c7ac931323f3d7bbad78ac4
Wei Kheng Teh <68549377+Wkt8@users.noreply.github.com>
2024-03-19 14:56:01 +0000

    This commit will add the scripts for getting the redcap schema and redcap data from redcap, and interim json files for redcap schema and data

---
+++ scripts/redcap_data.py
@@ -0,0 +1,22 @@
+
+import requests
+import json
+
+data = {
+    'token': '-',
+    'content': 'report',
+    'format': 'json',
+    'report_id': '1065',
+    'csvDelimiter': '',
+    'rawOrLabel': 'label',
+    'rawOrLabelHeaders': 'label',
+    'exportCheckboxLabel': 'true',
+    'returnFormat': 'json'
+}
+r = requests.post('https://redcap.h3abionet.org/redcap/api/',data=data)
+print('HTTP Status: ' + str(r.status_code))
+print(r.json())
+
+records = r.json()
+with open('../JSON_endpoints/redcap_data.json', 'w') as f:
+    json.dump(records, f, indent=4, separators=(',', ': '))
