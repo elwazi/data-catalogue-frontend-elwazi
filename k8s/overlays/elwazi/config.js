6e1811cc14c7a1314a5acc2e46b53244a759a646
Amnon Khen <amnon@ebi.ac.uk>
2024-04-02 12:50:21 +0100

    dc-30 catalogue, k8s, nginx config files for elwazi demo

--- k8s/overlays/elwazi/config.js
+++ k8s/overlays/elwazi/config.js
@@ -1,24 +1,31 @@
 window.config = {
-    "ENVIRONMENT":"demo",
-    "REST_ENDPOINT_URL": 'https://raw.githubusercontent.com/ebi-ait/data-catalogue-frontend-elwazi/main/data/catalogue-data.json',
+    "ENVIRONMENT":"elwazi",
+    "basename": "/catalogue/elwazi",
+    "REST_ENDPOINT_URL": '/api/redcap_data.json',
     "SCHEMA_ENDPOINT_URL": 'https://raw.githubusercontent.com/ebi-ait/data-catalogue-frontend-elwazi/main/data/catalogue-schema.json',
     "RESOURCE_JSON_PATH": '',
     "GRID_CONFIG":  [
         {
-            "name": "cohort_name",
+            "name": "d_status",
         },
         {
-            "name": "website",
+            "name": "d_category",
         },
         {
-            "name": "countries"
+            "name": "d_name"
         },
         {
-            "name": "available_data_types.climate_data"
+            "name": "d_description"
         },
         {
-            "name": "demographic",
-            "valueGetter": params => params.data.available_data_types.environmental
+            "name": "d_type"
+        }
+    ],
+    FILTER_FIELDS: [
+        {
+            field: "d_category",
+            type: "checkbox",
+            data_type: "string"
         }
     ]
 }
