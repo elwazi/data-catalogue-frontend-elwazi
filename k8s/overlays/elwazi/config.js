4b0db58bcc4fbe09b29e5d238e6187f984f6f093
Wei Kheng Teh <68549377+Wkt8@users.noreply.github.com>
2024-04-03 11:57:19 +0100

    This commit will update the demo config.js and redcap_data.json files for the eLwazi demo in april 24

--- k8s/overlays/elwazi/config.js
+++ k8s/overlays/elwazi/config.js
@@ -6,6 +6,9 @@ window.appConfig = {
     "RESOURCE_JSON_PATH": '',
     "GRID_CONFIG":  [
         {
+            "name": "p_title"
+        },
+        {
             "name": "d_status",
         },
         {
@@ -15,15 +18,38 @@ window.appConfig = {
             "name": "d_name"
         },
         {
-            "name": "d_description"
+            "name": "d_type"
         },
         {
-            "name": "d_type"
+            "name": "sample_size"
+        },
+        {
+            "name": "data_use_permission"
         }
     ],
     FILTER_FIELDS: [
         {
             field: "d_category",
+            type: "checkbox",
+            data_type: "string"
+        }
+        {
+            field: "d_status",
+            type: "checkbox",
+            data_type: "string"
+        }
+        {
+            field: "p_title",
+            type: "checkbox",
+            data_type: "string"
+        }
+        {
+            field: "d_type",
+            type: "checkbox",
+            data_type: "string"
+        }
+        {
+            field: "data_use_permission",
             type: "checkbox",
             data_type: "string"
         }
