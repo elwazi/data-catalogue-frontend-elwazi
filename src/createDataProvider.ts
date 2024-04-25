f533a6a0bf8339278b0696ed96f02740b01c2ad9
Amnon Khen <amnon@ebi.ac.uk>
2024-04-25 09:57:27 +0100

    dc-30 readable code for dev time

--- src/createDataProvider.ts
+++ src/createDataProvider.ts
@@ -2,7 +2,14 @@ import fakeDataProvider from "ra-data-fakerest";
 import {fetchUtils} from "react-admin";
 import {API_DATA_PATH} from "./constants";
 const httpClient = fetchUtils.fetchJson;
-const apiUrl = API_DATA_PATH;
+
+function buildApiUrl() {
+    const baseUriNoTrailingSlash = document.baseURI.endsWith('/')?document.baseURI.slice(0, -1):document.baseURI;
+    const apiUrl = (API_DATA_PATH.startsWith('http') ? '': baseUriNoTrailingSlash) + API_DATA_PATH;
+    return apiUrl;
+}
+
+const apiUrl = buildApiUrl();
 
 const groupBy = (array, key) => array.reduce((acc, obj) => ((acc[obj[key]] = acc[obj[key]] || []).push(obj), acc), {});
 
