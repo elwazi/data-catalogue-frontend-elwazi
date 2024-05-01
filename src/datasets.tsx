8693d3edc275abd8b6b367da4bc5ae0baf0a5b19
Amnon Khen <amnon@ebi.ac.uk>
2024-05-01 20:10:00 +0100

    dc-16 narrow list for narrow screens

--- src/datasets.tsx
+++ src/datasets.tsx
@@ -1,18 +1,24 @@
-import {Datagrid, DatagridConfigurable, List, SelectColumnsButton, TextField, TopToolbar} from "react-admin";
+import {
+    DatagridConfigurable,
+    FilterLiveSearch,
+    List,
+    SavedQueriesList,
+    SelectColumnsButton,
+    TextField,
+    TopToolbar
+} from "react-admin";
 
-// TODO this should come from a module becuase it would be shared by other catalogues
+// TODO this should come from a module because it would be shared by other catalogues
 import {FieldValuesFilter} from './FieldValuesFilter';
-import {Card, CardContent} from '@mui/material';
-import {FilterLiveSearch, SavedQueriesList, TextInput} from "react-admin";
+import {Card, CardContent, Theme, useMediaQuery} from '@mui/material';
 import React from "react";
-import {ExportButton, FilterButton} from "ra-ui-materialui";
-import {SearchInput} from "ra-ui-materialui";
+import {ExportButton, SimpleList} from "ra-ui-materialui";
 
 const FilterSidebar = () => (
-    <Card sx={{ order: -1}}>
+    <Card sx={{order: -1}}>
         <CardContent>
-            <SavedQueriesList />
-            <FilterLiveSearch />
+            <SavedQueriesList/>
+            <FilterLiveSearch/>
             <FieldValuesFilter column="d_category"/>
             <FieldValuesFilter column="d_type"/>
             <FieldValuesFilter column="d_status"/>
@@ -22,25 +28,37 @@ const FilterSidebar = () => (
 );
 const ListActions = () => (
     <TopToolbar>
-        <SelectColumnsButton />
+        <SelectColumnsButton/>
         <ExportButton/>
     </TopToolbar>
 );
 
 export const DatasetList = () => {
+    const isSmall = useMediaQuery<Theme>((theme) => theme.breakpoints.down("sm"));
+
     return (
         <List
-            actions={<ListActions />}
+            actions={<ListActions/>}
             aside={<FilterSidebar/>}
         >
-            <DatagridConfigurable>
-                <TextField source="d_name"/>
-                <TextField source="d_category"/>
-                <TextField source="d_type"/>
-                <TextField source="d_status"/>
-                <TextField source="sample_size"/>
-                <TextField source="data_use_permission"/>
-            </DatagridConfigurable>
+            {
+                isSmall ? (
+                    <SimpleList
+                        primaryText={(record) => record.d_name}
+                        secondaryText={(record) => record.d_category}
+                        tertiaryText={(record) => record.d_status}
+                    />
+                ) : (
+                    <DatagridConfigurable>
+                        <TextField source="d_name"/>
+                        <TextField source="d_category"/>
+                        <TextField source="d_type"/>
+                        <TextField source="d_status"/>
+                        <TextField source="sample_size"/>
+                        <TextField source="data_use_permission"/>
+                    </DatagridConfigurable>
+                )
+            }
         </List>
-    )
+    );
 };
