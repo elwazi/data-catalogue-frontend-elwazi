b998b22b025e2a6f72643a5296950061a9aee68b
Ziyaad Parker <ziyaadparker@yahoo.co.uk>
2025-03-14 16:37:23 +0200

    afrigen logo

--- src/MyAppBar.tsx
+++ src/MyAppBar.tsx
@@ -1,14 +1,28 @@
 import {AppBar, TitlePortal} from 'react-admin';
 import ElwaziLogo from "./elwaziLogo";
-import {Box} from "@mui/material";
+import {Box, styled} from "@mui/material";
 import DSIAfricaLogo from "./dsiAfricaLogo";
+import AfrigenLogo from "./afrigenLogo";
 
+// Create a styled wrapper for ElwaziLogo to enforce white color
+const WhiteElwaziLogo = styled(ElwaziLogo)({
+    '& path, & svg': {
+        fill: '#ffffff !important'
+    }
+});
+
+const WhiteAfrigenLogo = styled(AfrigenLogo)({
+    '& path, & svg': {
+        fill: '#ffffff !important'
+    }
+});
 
 export const MyAppBar = () => (
     <AppBar>
         <TitlePortal/>
         <Box component="span" flex={1}/>
-        <ElwaziLogo/>
+        <WhiteElwaziLogo/>
+        <WhiteAfrigenLogo/>
         <Box component="span" flex={1}/>
         <DSIAfricaLogo/>
         <Box component="span" flex={1}/>
